c38a66c994735c83fa8f30077b1bff25f6830bb0
= <=>
2024-06-21 17:42:08 +0100

    feat: experience section completed

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.html"],
+  darkMode: "selector",
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Figtree", "sans-serif"],
+      },
+      colors: {
+        primary: "#0076B6",
+        white: "rgba(229, 241, 247)",
+        black: "#010B13",
+        grey: "#BEBFC5",
+        border: "rgba(var(--color-primary), .1)",
+        // primary: "rgb(var(--color-primary))",
+        // white: "rgb(var(--color-white))",
+        // grey: "rgb(var(--color-grey))",
+        // black: "rgb(var(--color-black))",
+        // border: "rgb(var(--color-border))",
+      },
+      borderRadius: {
+        DEFAULT: "10px",
+      },
+    },
+  },
+  plugins: [],
+};
+
