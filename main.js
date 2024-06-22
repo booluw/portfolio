f2e6d05b481aa5bc2b4ebc9a2913a0a2eafa03a7
= <=>
2024-06-22 02:01:13 +0100

    feat: theme switcher active state

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 const theme_switcher = document.querySelectorAll("[data-theme]");
 
+const switchTheme = function () {
+  document.documentElement.className = localStorage.getItem("theme");
+  theme_switcher.forEach((el) => {
+    if (el.dataset.theme !== localStorage.getItem("theme")) {
+      el.classList.remove("theme-active");
+    } else {
+      el.classList.add("theme-active");
+    }
+  });
+}
+
 theme_switcher.forEach((item) => {
+
   item.addEventListener("click", (e) => {
     document.documentElement.className = e.target.dataset.theme;
-    // console.log(e.target.dataset.theme);
+    localStorage.setItem("theme", e.target.dataset.theme);
+    switchTheme();
+
   });
 });
+
+switchTheme();
